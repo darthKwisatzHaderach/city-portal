92d8c25fce38058211e380cb0e8a9adf7c44b755
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-02-10 18:07:50 +0500

    [CP-0003] - Check constraints

--- spec/model/panoramas_spec.rb
+++ spec/model/panoramas_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 describe Panoramas do
-  it 'panoramas should have path' do
+  it 'panoramas should have image path' do
     panorama = Panoramas.new(id: 29, path: '12', year: 2014, name: '12-этажка')
+    expect { panorama.save }.to raise_error
+  end
+
+  it 'panoramas should have path' do
+    panorama = Panoramas.new(id: 29, year: 2014, img_path: '/panoramas/12_preview.jpg', name: '12-этажка')
     expect { panorama.save }.to raise_error
   end
 end
